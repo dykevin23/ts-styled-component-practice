d3a82f13d97e36b9b1097f85490ca6822baef8b9
yeond23k <dyk0203@naver.com>
2022-03-29 00:52:03 +0900

    practice styled-component with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Circle from "./Circle";
 
 function App() {
+
+  const [inputValue, setInputValue] = useState('')
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setInputValue(event.currentTarget.value)
+  }
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    console.log("### hello " + inputValue)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Circle borderColor={'yellow'} bgColor={'teal'}/>
+      <Circle bgColor={'tomato'}/>
+
+      <form onSubmit={onSubmit}>
+        <input value={inputValue} type="text" placeholder="username" onChange={onChange}/>
+        <button>submit</button>
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
